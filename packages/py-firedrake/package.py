dae9cf9a50aa495fdd0a19c080e62604021642cb
Connor Ward <c.ward20@imperial.ac.uk>
2021-03-30 18:39:50 +0100

    Editable installs\!

--- packages/py-firedrake/package.py
+++ packages/py-firedrake/package.py
@@ -18,4 +18,20 @@ class PyFiredrake(PythonPackage):
 
     depends_on('python@3.6:')
     depends_on('py-setuptools', type='build')
-    depends_on('firedrake.petsc')
+    #depends_on('firedrake.petsc')
+
+    def install(self, spec, prefix):
+        # import pdb; pdb.set_trace()
+        #print(type(spec))
+        # print(type(spec["py-firedrake"]))
+        # print(dir(spec["py-firedrake"]))
+        if "dev_path" in spec["py-firedrake"].variants:
+            self.setup_py("develop")
+        else:
+            self.setup_py("install")
+        # print(spec["py-firedrake"].extra_attributes)
+        #print(spec.to_dict())
+        #print(spec.to_dict()["spec"][0]["py-firedrake"]["parameters"]["dev_path"])#["py-firedrake"]["parameters"])
+        #print("dev_path" in spec)
+        #print(repr(spec))
+        
